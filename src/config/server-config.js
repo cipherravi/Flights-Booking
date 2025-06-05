b4724fe36248e0fc23c287417cf3e2e08105798b
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-06-05 11:11:06 +0530

    Fix: config file for server

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,6 +1,7 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
+const path = require("path");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+}
 
 module.exports = {
   PORT: process.env.PORT,
